9ccc2a8fe959bc01c3eeecb65a45140cb12140b7
SgtGarro <marcelogarro137@gmail.com>
2023-05-10 16:19:12 -0500

    state & useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,20 @@
+import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log(`Los usuarios buscan todos de ${searchValue}`);
+
   return (
     <div className="todo-search">
       <input
         className="todo-input"
         placeholder="Cortar cebolla"
         type="text"
-        onChange={function (event) {
-          console.log(event);
-          console.log(event.target);
-          console.log(event.target.value);
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value);
         }}
       />
       <svg
